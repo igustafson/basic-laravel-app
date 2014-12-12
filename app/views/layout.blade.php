88032c5a227c11e427b5588fbbb81a99bfbe78bd
Ian Gustafson <igustafson@inthepinkonline.com>
2014-12-12 15:12:52 -0500

    Minor styling
     * Added body padding
     * Introduced bootstrap and added some basic bootstrap style classes
     * Removed backend limit on display of address field

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,7 +1,27 @@
-<html>
-    <body>
-        <h1>Basic Laravel App</h1>
+<!doctype html>
 
-        @yield('content')
-    </body>
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8">
+
+    <title>Basic Larivel App by Ian Gustafson</title>
+
+    {{ HTML::style('http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css') }}
+    {{ HTML::style('http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css') }}
+    {{ HTML::style('css/app.css') }}
+    {{ HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js') }}
+    {{ HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js') }}
+
+  </head>
+
+  <body>
+
+    <h1>Basic Laravel App</h1>
+
+    @yield('content')
+
+  </body>
+
 </html>
--- app/views/stores.blade.php
+++ app/views/stores.blade.php
@@ -4,17 +4,23 @@
 
     <h2>Stores</h2>
 
-    {{ Form::open(array('action' => 'StoreController@showList', 'method' => 'get')) }}
-      {{ Form::label('company_id', 'Company') }}
-      {{ Form::select('company_id', array('all' => '- All -')+$company_options, $list_filters['company_id']) }}
-      {{ Form::label('city', 'Search by City') }}
-      {{ Form::text('city', $list_filters['city']) }}
-      {{ Form::label('address', 'Search by Address') }}
-      {{ Form::text('address', $list_filters['address']) }}
-      {{ Form::submit('Apply Filters') }}
-    {{ Form::close() }}
+    <nav class="navbar navbar-default">
+      <div class="fluid">
+        {{ Form::open(array('action' => 'StoreController@showList', 'method' => 'get', 'class' => 'navbar-form navbar-left', 'role' => 'search')) }}
+          {{ Form::label('company_id', 'Company') }}
+          {{ Form::select('company_id', array('all' => '- All -')+$company_options, $list_filters['company_id']) }}
+          {{ Form::label('city', 'City') }}
+          {{ Form::text('city', $list_filters['city']) }}
+          {{ Form::label('address', 'Address') }}
+          {{ Form::text('address', $list_filters['address']) }}
+          {{ Form::submit('Apply Filters') }}
+        {{ Form::close() }}
+      </div>
+    </nav>
 
-    <table>
+    <div class="text-center">{{ $stores->appends(Input::except(array('page')))->links() }}</div>
+
+    <table class="table table-striped">
 
       <thead>
         <tr>
@@ -30,7 +36,7 @@
       @forelse($stores as $store)
         <tr>
           <td>{{{ is_null($store->company) ? '' : $store->company->name }}}</td>
-          <td>{{{ str_limit($store->address, 50) }}}</td>
+          <td>{{{ $store->address }}}</td>
           <td>{{{ $store->city }}}</td>
           <td>{{{ $store->state }}}</td>
           <td>{{{ $store->postal_code }}}</td>
@@ -44,6 +50,6 @@
 
     </table>
 
-    {{ $stores->appends(Input::except(array('page')))->links() }}
+    <div class="text-center">{{ $stores->appends(Input::except(array('page')))->links() }}</div>
 
 @stop
